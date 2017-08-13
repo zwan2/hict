13bbe6adc436bb6b76587164a450712f7dcdd771
zwan2 <2zwan@naver.com>
2017-08-14 00:17:44 +0900

    170814.1
    -input size 조절
    -login관련 페이지 정리 (login_main.html)
    -소개 페이지 분리
    -footer ++
    -calendar 종료시간X

--- account_delete.php
+++ account_delete.php
@@ -20,21 +20,21 @@ if($result = $db->query($query)) {
 <form method="post" action="account_delete2.php" onsubmit="return password_check();" name="account" class="form-horizontal">
 	<input type="hidden" name="crypt_password" value="<?=$password?>">
 	<div class="form-group">
-		<div class="col-xs-10">
+		<div class="col-xs-12">
 			<label for="password">비밀번호</label>
 			<input type="password" class="form-control" name="password" placeholder="영문, 숫자 혼용 8-20자리" maxlength="20">
 		</div>
 	</div>
 
 	<div class="form-group">
-		<div class="col-xs-10">
+		<div class="col-xs-12">
 			<label for="password2">비밀번호 확인</label>
 			<input type="password" class="form-control" name="password2" maxlength="20">
 		</div>
 	</div>
 
 	<div class="form-group">
-    <div class="col-xs-10">
+    <div class="col-xs-12">
       <input class="btn btn-default" type="submit" onclick="return confirm('정말 탈퇴하시겠습니까?');" value="탈퇴">
     </div>
   </div>
