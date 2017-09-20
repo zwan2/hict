af260b369ee7793e08264ee247eeb39822ac2ee5
zwan2 <2zwan@naver.com>
2017-09-20 14:17:05 +0900

    170920 1416
    PUSH SDK 설치

--- bookinglist.php
+++ bookinglist.php
@@ -97,7 +97,35 @@
     </div><!-- /.col-lg-6 -->
   </div><!-- /.row -->
 
+  <!-- PUSH -->
+  <a href="#" id="subscribe-link" style="display: none;">Subscribe to Notifications</a>
+  <script>
+      function subscribe() {
+          OneSignal.push(["registerForPushNotifications"]);
+          event.preventDefault();
+      }
+
+      var OneSignal = OneSignal || [];
+      /* This example assumes you've already initialized OneSignal */
+      OneSignal.push(function() {
+          // If we're on an unsupported browser, do nothing
+          if (!OneSignal.isPushNotificationsSupported()) {
+              return;
+          }
+          OneSignal.isPushNotificationsEnabled(function(isEnabled) {
+              if (isEnabled) {
+                  // The user is subscribed to notifications
+                  // Don't show anything
+              } else {
+                  document.getElementById("subscribe-link").addEventListener('click', subscribe);
+                  document.getElementById("subscribe-link").style.display = '';
+              }
+          });
+      });
+  </script>
+
 </div>
+
 
 
 
